ed044c3844ebd7cb44a5f7e2b554df5517a9b95a
Drew <djnevins@uw.edu>
2020-04-20 19:56:18 -0400

    wrote first part of function to return page extension type. partially working, need to look at variable scope.

--- js/request-logic.js
+++ js/request-logic.js
@@ -3,6 +3,21 @@
 // the function to use it! We hope this is useful to you!
 
 
+filename = "readme.html"
+
 function getContentType(filename) {
   const extension = filename.match(/.*\.([^\.]*)$/)[1];
+
+  function showContentType(extension) {
+    if (extension === 'text/html') {
+      console.log('It\'s html');
+    } else {
+      console.log('no idea');
+    }
+  }
+showContentType(extension);
 }
+getContentType(filename);
+
+
+
